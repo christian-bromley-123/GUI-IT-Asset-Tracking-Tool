67b91be1022d5471d5032296a8a6e615f19a2975
christian-bromley-123 <christian.bromley@gmail.com>
2025-06-13 11:06:42 -0700

    -Set layout for main viewing window

--- include/DBViewer.hpp
+++ include/DBViewer.hpp
@@ -8,5 +8,16 @@ class DBViewer : public wxFrame
 {
 public:
 	DBViewer(const wxString& title, SQLHENV& hEnv, SQLHDBC& hDbc, SQLHSTMT& hStmt, bool isTestServer);
+
+private:
+	void createControls();
+	void setupSizers();
+	void bindEvents();
+
+	wxPanel* m_topPanel;
+	wxPanel* m_tablePanel;
+	wxPanel* m_searchPanel;
+	wxPanel* m_viewPanel;
+	wxPanel* m_controlPanel;
 };
 
--- src/DBViewer.cpp
+++ src/DBViewer.cpp
@@ -10,40 +10,48 @@
 DBViewer::DBViewer(const wxString& title, SQLHENV& hEnv, SQLHDBC& hDbc, SQLHSTMT& hStmt, bool isTestServer) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize)
 
 {
-	// Execute SELECT
-	std::wstring testSelect = L"SELECT [Device_Model_Name] as Model,[Device_Model_Type] as Type FROM [Device_Models]";
-	int retcode = SQLExecDirect(hStmt, (wchar_t*)testSelect.c_str(), SQL_NTS);
-
-	// Get number of columns and rows
-	long long rowCount;
-	SQLRowCount(hStmt, &rowCount);
-	
-	short columnCount;
-	SQLNumResultCols(hStmt, &columnCount);
-
-	// Get column names
-	std::vector<std::wstring> colNames = dbfunctions::getAllResultColumnNames(hStmt);
-	
-	// Get table of results
-	std::vector<std::vector<std::wstring>> tableData = dbfunctions::getResultTable(hStmt);
-
-
-	// Building Frame
-	wxPanel* panel = new wxPanel(this);
-	wxGrid* grid = new wxGrid(panel, -1, wxPoint(0, 0), wxSize(400, 1000));
-	grid->CreateGrid(rowCount, columnCount);
-	grid->EnableEditing(false);
-	grid->HideRowLabels();
-	for (int i=0; i<columnCount;i++) {
-		grid->SetColLabelValue(i, colNames[i]);
-	}
-
-	for (int i=0; i< columnCount; i++) {
-		for (int j = 0; j<tableData.size(); j++) {
-			grid->SetCellValue(j, i, tableData[j][i]);
-		}
-	}
-
-	grid->AutoSize();
-
+	createControls();
+	setupSizers();
+	bindEvents();
 };
+
+void DBViewer::createControls() {
+	m_topPanel = new wxPanel(this);
+	m_topPanel->SetBackgroundColour(wxColor(0, 0, 0)); // Black
+
+	m_tablePanel = new wxPanel(m_topPanel, wxID_ANY, wxDefaultPosition, wxSize(200,400));
+	m_tablePanel->SetBackgroundColour(wxColor(100,100,100)); // Dark gray
+
+	m_searchPanel = new wxPanel(m_topPanel, wxID_ANY, wxDefaultPosition, wxSize(200, 40));
+	m_searchPanel->SetBackgroundColour(wxColor(100, 0, 100)); // Purple
+
+	m_viewPanel = new wxPanel(m_topPanel, wxID_ANY, wxDefaultPosition, wxSize(300,300));
+	m_viewPanel->SetBackgroundColour(wxColor(300, 200, 100)); // Green
+
+	m_controlPanel = new wxPanel(m_topPanel, wxID_ANY, wxDefaultPosition, wxSize(100,300));
+	m_controlPanel->SetBackgroundColour(wxColor(100, 300, 200));
+
+
+}
+
+void DBViewer::setupSizers() {
+	wxBoxSizer* topSizer = new wxBoxSizer(wxHORIZONTAL);
+	topSizer->Add(m_tablePanel, wxSizerFlags().Expand());
+	m_topPanel->SetSizer(topSizer);
+
+	wxBoxSizer* midSizer = new wxBoxSizer(wxVERTICAL);
+	midSizer->Add(m_searchPanel, wxSizerFlags().Expand());
+	topSizer->Add(midSizer, wxSizerFlags().Expand().Proportion(1));
+
+	wxSizer* lowSizer = new wxBoxSizer(wxHORIZONTAL);
+	lowSizer->Add(m_viewPanel, wxSizerFlags().Expand().Proportion(1));
+	lowSizer->Add(m_controlPanel, wxSizerFlags().Expand());
+	midSizer->Add(lowSizer, wxSizerFlags().Expand().Proportion(1));
+
+	topSizer->SetSizeHints(this);
+
+}
+
+void DBViewer::bindEvents() {
+
+}
